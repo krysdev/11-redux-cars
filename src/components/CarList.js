1d121f2245a0701ee3ae85cd7a0d171d5a9132d7
KrysDev <webdevkrys@gmail.com>
2023-03-28 20:33:34 +0100

    x

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,30 +4,39 @@ import { removeCar } from '../store';
 function CarList() {
   const dispatch = useDispatch();
 
-  // const cars = useSelector( (state) => state.cars.data ); // so the BIG STATE is destructured below.
+  // const cars = useSelector( (state) => state.cars.data ); // so the BIG STATE (see at the bottom) is destructured below.
   // also 'cars' is used later to map through, so here is a good place to filter already what needs to be mapped
   // (we create a "derived state" - calculated from 2 small-states: data and searchTerm to create filtered cars )
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter(
+  // const {cars, name} -->  state.cars, state.form.name
+  const { cars, name1 } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter(
       (dataArrayEl) =>
         dataArrayEl.name.toLowerCase().includes(searchTerm.toLowerCase()) // true if the search term is part of the name
     );
+    return {
+      cars: filteredCars,
+      name1: form.name,
+    };
   });
 
   const handleCarDelete = (carRow) => {
     dispatch(removeCar(carRow.id));
   };
 
-  const renderedCars = cars.map((car) => {
-    //
+  const renderedCars = cars.map((carEl) => {
+    // name1 --> state.form.name
+    // if 'name1' is present/truthy then check if it matches (true/false)
+    const bold = name1 && carEl.name.toLowerCase().includes(name1.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      // if const bold is true then use class 'bold'
+      <div key={carEl.id} className={`panel ${bold && 'bold'}`}>
         <p>
-          {car.name} - € {car.cost}
+          {carEl.name} - € {carEl.cost}
         </p>
         <button
           className="button is-danger"
-          onClick={() => handleCarDelete(car)}
+          onClick={() => handleCarDelete(carEl)}
         >
           Delete
         </button>
@@ -47,6 +56,11 @@ export default CarList;
 
 // BIG STATE:
 //         {
+//            form: {
+//                   name: '',
+//                   cost: 0
+//                  },
+//
 //            cars: {
 //                   searchTerm: '',
 //                   data: [id: 11, name: 'ford', cost: 150]
